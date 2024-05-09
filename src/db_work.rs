3b707a19a37a2d7eba373b033ccd8823ad181d3e
himoji <ma.lyaka@bk.ru>
2024-05-09 19:49:20 +0500

    Implemented work with db

--- src/db_work.rs
+++ src/db_work.rs
@@ -1,39 +1,65 @@
-// use serde::{Deserialize, Serialize};
-// use surrealdb::engine::remote::ws::Client;
-// use surrealdb::sql::Thing;
-// use surrealdb::Surreal;
-// use crate::work::Work;
+use serde::{Deserialize, Serialize};
+use surrealdb::engine::remote::ws::Client;
+use surrealdb::sql::{Thing};
+use surrealdb::{Surreal};
+
+use crate::work::Work;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct UserData {
+    id: Thing,
+}
+impl UserData {
+    pub fn get_id(&self) -> String {
+        self.id.id.to_string()
+    }
+}
+
+const WORK_DB_NAME: &str = "work";
+
+// pub async fn add_filter_works_vec(db: &Surreal<Client>, vec: Vec<Work>) -> surrealdb::Result<()> {
+//     let a = Work::remove_duplicates(get_all_works(db).await?, vec);
+//     dbg!(&a);
 //
-// #[derive(Deserialize, Serialize, Debug)]
-// struct UserData {
-//     id: Thing,
-// }
+//     add_works_vec(db, a).await?;
 //
-// // pub async fn add_filter_works_vec(db: &Surreal<Client>, vec: Vec<Work>) -> surrealdb::Result<()> {
-// //     let a = Work::remove_duplicates(get_all_works(db).await?, vec);
-// //     dbg!(&a);
-// //
-// //     add_works_vec(db, a).await?;
-// //
-// //     Ok(())
-// // }
-// //
-// // pub async fn get_all_works(db: &Surreal<Client>) -> surrealdb::Result<Vec<Work>> {
-// //     let mut resp = db.query("select * from work").await?;
-// //     let old_vec: Vec<Work> = resp.take(0)?;
-// //     Ok(old_vec)
-// // }
-// //
-// // pub async fn add_works_vec(db: &Surreal<Client>, vec: Vec<Work>) -> surrealdb::Result<()> {
-// //     for work in vec {
-// //         let created: Vec<UserData> = db.create("work").content(work).await?;
-// //         dbg!(created);
-// //     }
-// //     Ok(())
-// // }
-//
-// pub async fn add_work(db: &Surreal<Client>, work: Work) -> surrealdb::Result<()> {
-//     let created: Vec<UserData> = db.create("work").content(work).await?;
-//     dbg!(created);
 //     Ok(())
 // }
+//
+// pub async fn get_all_works(db: &Surreal<Client>) -> surrealdb::Result<Vec<Work>> {
+//     let mut resp = db.query("select * from work").await?;
+//     let old_vec: Vec<Work> = resp.take(0)?;
+//     Ok(old_vec)
+// }
+//
+// pub async fn add_works_vec(db: &Surreal<Client>, vec: Vec<Work>) -> surrealdb::Result<()> {
+//     for work in vec {
+//         let created: Vec<UserData> = db.create("work").content(work).await?;
+//         dbg!(created);
+//     }
+//     Ok(())
+// }
+
+pub async fn add_work(db: &Surreal<Client>, work: Work) -> surrealdb::Result<String> {
+    let created: Vec<UserData> = db.create(WORK_DB_NAME).content(work).await?;
+    dbg!(created.first().clone());
+
+    let id = created.first().unwrap().get_id();
+
+    Ok(id)
+}
+
+pub async fn get_work(db: &Surreal<Client>, index: String) -> surrealdb::Result<Work> {
+    let acquired_work: Option<Work> = db.select((WORK_DB_NAME, index.clone())).await?;
+
+    match acquired_work {
+        None => {
+            panic!("[ERROR]: Failed to get work by index: {index} from the db.")
+        }
+        Some(work) => {
+            dbg!(work.clone());
+            Ok(work)
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod time_work;
 mod work;
 mod string_work;
 
+use surrealdb::engine::remote::ws::{Client, Ws};
+use surrealdb::Surreal;
 use tonic::{Request, Response, Status};
 use tonic::transport::Server;
 
@@ -17,36 +19,68 @@ mod proto {
         tonic::include_file_descriptor_set!("db_api_descriptor");
 }
 
-#[derive(Debug, Default)]
-struct DbService{}
+#[derive(Debug)]
+struct DbService{
+   db: Surreal<Client>
+}
 
 #[tonic::async_trait]
 impl DbApi for DbService {
     async fn add_work(&self, request: Request<ProtoWork>) -> Result<Response<ProtoWorkIndex>, Status> {
         let work = Work::from_request_work(request.get_ref().clone());
-        dbg!(work);
+        dbg!(work.clone());
+
+        let index = match db_work::add_work(&self.db, work).await {
+            Err(err) => {
+                format!("[ERROR]: Failed to add work to database: {}", err);
+                return Err(Status::internal("Internal server error: Database operation failed"));
+            },
+            Ok(index) => {index}
+        };
 
         let resp = ProtoWorkIndex {
-            index: 0 //todo!(implement surrealdb or tikv)
+            index
         };
 
         Ok(Response::new(resp))
     }
 
     async fn get_work(&self, request: Request<ProtoWorkIndex>) -> Result<Response<ProtoWork>, Status> {
-        todo!()
+        let index = request.into_inner().index;
+
+        let work = match db_work::get_work(&self.db, index).await {
+            Err(err) => {
+                format!("[ERROR]: Failed to get work from database: {}", err);
+                return Err(Status::internal("Internal server error: Database operation failed"));
+            },
+            Ok(work) => {work}
+        };
+
+        dbg!(work.clone());
+
+        let resp = ProtoWork{
+            name: work.name,
+            desc: work.desc,
+            date_start: work.date_start,
+            date_end: work.date_end,
+        };
+
+        Ok(Response::new(resp))
     }
 }
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+    db.use_ns("test").use_db("test").await?;
+
     let addr = "[::1]:50051".parse()?;
     let service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
         .build()?;
 
-    let db = DbService::default();
+    let db = DbService { db };
 
     println!("{}", addr);
     Server::builder()
